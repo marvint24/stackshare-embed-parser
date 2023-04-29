6c3a15e2fac1726ba6971ad5d3857bec5e402681
marvint24 <marvin.terme@gmail.com>
2023-04-29 20:10:55 +0200

    changed to function

--- src/main.ts
+++ src/main.ts
@@ -14,37 +14,36 @@ function replaceNewLines(str: string | undefined | null) {
   return str.replace(/(\r\n|\n|\r)/gm, "")
 }
 
-let JSONResult: StackShareCategory[] = []
+export default async function parseStackShare(url: string): Promise<StackShareCategory[]> {
+  let JSONResult: StackShareCategory[] = []
 
-let stackshareResponse = await fetch("https://embed.stackshare.io/stacks/embed/3bd65ed6468388c08b3c0a8bd772f1")
-let stackshareHtml = await stackshareResponse.text()
-let dom = new jsdom.JSDOM(stackshareHtml)
-let categories = dom.window.document.querySelectorAll(".layer-wrapper")
+  let stackshareResponse = await fetch(url)
+  let stackshareHtml = await stackshareResponse.text()
+  if (!stackshareHtml) throw new Error("No HTML response from StackShare")
+  let dom = new jsdom.JSDOM(stackshareHtml)
+  let categories = dom.window.document.querySelectorAll(".layer-wrapper")
 
-let data = 0
-for (let category of categories) {
-  let tools = category.querySelectorAll(".service-function-wrapper")
-  for (let tool of tools) {
-    let name = tool.querySelector("span.stack-service-name-under")?.textContent
-    name = replaceNewLines(name)
+  for (let category of categories) {
+    let tools = category.querySelectorAll(".service-function-wrapper")
+    for (let tool of tools) {
+      let name = tool.querySelector("span.stack-service-name-under")?.textContent
+      name = replaceNewLines(name)
 
-    let type = tool.querySelector("div.function-name-under")?.textContent
-    type = replaceNewLines(type)
+      let type = tool.querySelector("div.function-name-under")?.textContent
+      type = replaceNewLines(type)
 
-    let imgUrl = tool.querySelector("div.stack-service-logo>img")?.getAttribute("src")
-    let base64img: string | null = null
+      let imgUrl = tool.querySelector("div.stack-service-logo>img")?.getAttribute("src")
+      let base64img: string | null = null
 
-    if (!imgUrl) {
-      base64img = null
-    } else {
-      let imageData = await fetch(imgUrl)
-      let buffer = await imageData.arrayBuffer()
-      base64img = Buffer.from(buffer).toString("base64")
-    }
-
-    if (base64img) {
-      data += base64img?.length
+      if (!imgUrl) {
+        base64img = null
+      } else {
+        let imageData = await fetch(imgUrl)
+        let buffer = await imageData.arrayBuffer()
+        base64img = Buffer.from(buffer).toString("base64")
+      }
     }
   }
+
+  return JSONResult
 }
-console.log(data)
